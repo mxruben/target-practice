32a53324c007d6b98e68e9a79003c0773e88372c
Maxwell Ruben <maxruben1@outlook.com>
2026-04-28 21:39:46 -0500

    Refactored and fixed cleanup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Kitboga Creation Jam 2026
 An advertisement interface that prompts the user to click on a certain amount of
 targets before they can skip.
+
+Configuration is at the top of ad.js. Default is hard but doable. 
--- submission/ad.js
+++ submission/ad.js
@@ -2,35 +2,145 @@
 const targetCount = 16;
 const targetTimeSeconds = 10;
 
-const hitSounds = [
-    new Audio('assets/hit1.ogg'),
-    new Audio('assets/hit2.ogg'),
-    new Audio('assets/hit3.ogg'),
-    new Audio('assets/hit4.ogg'),
-    new Audio('assets/hit5.ogg'),
-    new Audio('assets/hit6.ogg'),
-    new Audio('assets/hit7.ogg'),
-    new Audio('assets/hit8.ogg'),
-];
+class Game {
+    // Elements
+    #target = document.getElementById('target');
+    #timer = document.getElementById('timer');
+    #instructions = document.getElementById('instructions');
+    #counter = document.getElementById('counter');
+
+    // Bounds of target
+    #targetMaxX = 0;
+    #targetMinY = 0;
+    #targetMaxY = 0;
+
+    // Runtime variables
+    #timeRemaining = targetTimeSeconds;
+    #timerIntervalId = null;
+    #clickCount = 0;
+
+    // Labels
+    #timerLabel = 'Time: ';
+    #counterLabel = 'Targets left: ';
+
+    #hitSounds = [
+        new Audio('assets/hit1.ogg'),
+        new Audio('assets/hit2.ogg'),
+        new Audio('assets/hit3.ogg'),
+        new Audio('assets/hit4.ogg'),
+        new Audio('assets/hit5.ogg'),
+        new Audio('assets/hit6.ogg'),
+        new Audio('assets/hit7.ogg'),
+        new Audio('assets/hit8.ogg'),
+    ];
+
+    #targetAnimation = [
+        { transform: 'scale(1)' },
+        { transform: 'scale(0.9)' },
+        { transform: 'scale(1)' },
+    ];
+
+    #targetRotationTiming = {
+        duration: 1000,
+        iterations: targetTimeSeconds
+    };
+
+    start() {
+        this.initializeState();
+        this.#target.addEventListener('click', this.onTarget.bind(this));
+        this.#timerIntervalId = setInterval(this.updateTimer.bind(this), 1000);
+    }
+
+    initializeState() {
+        // Instructions
+        this.#instructions.style.display = 'block';
+
+        // Timer
+        this.#timer.style.display = 'block';
+        this.#timer.textContent = this.#timerLabel + this.#timeRemaining;
+
+        // Counter
+        this.#counter.style.display = 'block';
+        this.#counter.textContent = this.#counterLabel + targetCount;
+
+        // Target
+        this.#target.style.display = 'block';
+        this.animateTarget();
+        // Target bounds
+        this.#targetMaxX = document.getElementById('overlay-container').offsetWidth - this.#target.naturalWidth;
+        this.#targetMinY = this.#instructions.offsetTop + this.#instructions.offsetHeight;
+        this.#targetMaxY = this.#timer.offsetTop - this.#target.naturalHeight;
+
+        this.moveTarget();
+    }
+
+    // Moves target to random location.
+    moveTarget() {
+        let x = Math.random() * this.#targetMaxX;
+        let y = Math.random() * (this.#targetMaxY - this.#targetMinY) + this.#targetMinY;
+        
+        this.#target.style.left = x + 'px';
+        this.#target.style.top = y + 'px';
+    }
+
+    cleanup() {
+        this.#target.removeEventListener('click', this.onTarget);
+        this.#target.style.display = "none";
+        this.#timer.style.display = "none";
+        this.#instructions.style.display = "none";
+        this.#counter.style.display = 'none';
+        if (this.#timerIntervalId != null) {
+            clearInterval(this.#timerIntervalId);
+        }
+    }
+
+    playHitSound() {
+        this.#hitSounds[this.#clickCount % this.#hitSounds.length].play();
+    }
+
+    animateTarget() {
+        this.#target.animate(this.#targetAnimation, this.#targetRotationTiming);
+    }
+
+    onTarget() {
+        this.playHitSound();
+        this.#clickCount++;
+        this.#counter.textContent = this.#counterLabel + (targetCount - this.#clickCount);
+        if (this.#clickCount >= targetCount) {
+            this.cleanup();
+            adSuccess();
+            return;
+        }
+        this.moveTarget();
+    }
+
+    updateTimer() {
+        this.#timeRemaining--;
+        if (this.#timeRemaining < 0) {
+            this.cleanup();
+            adFail();
+            return;
+        }
+        this.#timer.textContent = this.#timerLabel + this.#timeRemaining;
+    }
+}
 
 (function init() {
-    createListeners();
+    const game = new Game;
+    createListeners(game);
 })()
 
-function createListeners() {
-    window.addEventListener('message', onMessage);
-    const skipButton = document.getElementById('skip');
-    skipButton.addEventListener('click', onSkip);
+function createListeners(game) {
+    window.addEventListener('message', (event) => onMessage(event, game));
+    document.getElementById('skip').addEventListener('click', (event) => onSkip(event, game));
 }
 
-function onSkip(event) {
-    const instructions = document.getElementById('instructions');
+function onSkip(event, game) {
     event.target.style.display = 'none';
-    instructions.style.display = 'block';
-    startGame(targetCount, targetTimeSeconds);
+    game.start();
 }
 
-function onMessage(event) {
+function onMessage(event, game) {
     const skipButton = document.getElementById('skip');
     if (!event.data || !event.data.type) return;
     
@@ -44,6 +154,7 @@ function onMessage(event) {
     if (event.data.type === 'adFinished') {
         skipButton.style.display = 'none';
         window.top.postMessage({ type: 'fail' }, '*');
+        game.cleanup();
     }
 }
 
@@ -55,116 +166,4 @@ function adSuccess() {
 function adFail() {
     document.getElementById('skip').style.display = 'none';
     window.top.postMessage({ type: 'fail' }, '*');
-}
-
-function playHitSound(clickNumber) {
-    hitSounds[clickNumber % hitSounds.length].play();
-}
-
-const targetAnimation = [
-    { transform: "scale(1)" },
-    { transform: "scale(0.9)" },
-    { transform: "scale(1)" },
-];
-
-const elementRotationTiming = {
-    duration: 1000,
-    iterations: targetTimeSeconds
-};
-
-function rotateElement(element) {
-    element.animate(targetAnimation, elementRotationTiming);
-}
-
-function cleanup() {
-    const target = document.getElementById("target");
-    const timer = document.getElementById("timer");
-    const instructions = document.getElementById("instructions");
-    const counter = document.getElementById("counter");
-
-    target.removeEventListener('click', onTarget);
-    target.style.display = "none";
-    timer.style.display = "none";
-    instructions.style.display = "none";
-    counter.style.display = 'none';
-    if (timerIntervalId != null) {
-        clearInterval(timerIntervalId);
-    }
-}
-
-function startGame(targetCount, targetTimeMs) {
-    const overlayContainer = document.getElementById("overlay-container");
-    const target = document.getElementById("target");
-    const instructions = document.getElementById("instructions");
-    const timer = document.getElementById("timer");
-    const counter = document.getElementById("counter");
-    
-    // Bounds of target.
-    const maxX = overlayContainer.offsetWidth - target.naturalWidth;
-    const minY = instructions.offsetTop + instructions.offsetHeight;
-    const maxY = overlayContainer.offsetHeight - target.naturalHeight;
-    
-    // Initial state.
-    moveTarget();
-    
-    target.style.display = 'block';
-    rotateElement(target);
-    
-    timer.style.display = 'block';
-    const timerLabel = 'Time: ';
-    timer.textContent = timerLabel + targetTimeSeconds;
-    let time = targetTimeSeconds;
-    let timerIntervalId = null;
-    
-    counter.style.display = 'block';
-    const counterLabel = 'Targets left: ';
-    counter.textContent = counterLabel + targetCount;
-    
-    let clicks = 0;
-    
-    function onTarget() {
-        playHitSound(clicks);
-        clicks++;
-        counter.textContent = counterLabel + (targetCount - clicks);
-        if (clicks >= targetCount) {
-            cleanup();
-            adSuccess();
-            return;
-        }
-        moveTarget();
-    }
-    
-    // Call when game ends.
-    function cleanup() {
-        target.removeEventListener('click', onTarget);
-        target.style.display = "none";
-        timer.style.display = "none";
-        instructions.style.display = "none";
-        counter.style.display = 'none';
-        if (timerIntervalId != null) {
-            clearInterval(timerIntervalId);
-        }
-    }
-    
-    function updateTimer() {
-        time--;
-        if (time < 0) {
-            cleanup();
-            adFail();
-            return;
-        }
-        timer.textContent = timerLabel + time;
-    }
-    
-    // Moves target to random location.
-    function moveTarget() {
-        let x = Math.random() * maxX;
-        let y = Math.random() * (maxY - minY) + minY;
-        
-        target.style.left = x + 'px';
-        target.style.top = y + 'px';
-    }
-    
-    target.addEventListener('click', onTarget);
-    timerIntervalId = setInterval(updateTimer, 1000);
 }
